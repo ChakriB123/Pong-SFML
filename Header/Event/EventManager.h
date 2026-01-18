b443489cbd799126028d50ad3e965d4df910237f
Chakradhar <chakribathala@gmail.com>
2026-01-18 13:26:38 +0530

    added mouse clicks and gives position of the click

--- Header/Event/EventManager.h
+++ Header/Event/EventManager.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+#include <iostream>
+using namespace std;
 using namespace sf;
 
  namespace Events {
@@ -7,6 +9,7 @@ using namespace sf;
      public:
          void pollEvents(RenderWindow* game_window); // Process all events
          bool isKeyPressed(sf::Keyboard::Key key);   // Check specific key
+         bool isLeftMouseButtonClicked();
      };
 
 }
--- Source/Event/EventManager.cpp
+++ Source/Event/EventManager.cpp
@@ -1,6 +1,4 @@
 #include "../../Header/Event/EventManager.h"
-
-
 namespace Events {
     void EventManager::pollEvents(RenderWindow* game_window) {
         sf::Event event;
@@ -9,10 +7,21 @@ namespace Events {
                 game_window->close();
 
             }
+            if (isLeftMouseButtonClicked())
+            {
+                sf::Vector2i position = sf::Mouse::getPosition(*game_window);
+
+                // Log the mouse position
+                 cout << "Left mouse click at: " << position.x << ", " << position.y << std::endl;
+            }
         }
     }
     bool EventManager::isKeyPressed(sf::Keyboard::Key key) {
         // Detect if a specific key is pressed
         return sf::Keyboard::isKeyPressed(key);
+    }
+    bool EventManager::isLeftMouseButtonClicked() {
+        return (sf::Mouse::isButtonPressed(sf::Mouse::Left));
+    
     }
 }
